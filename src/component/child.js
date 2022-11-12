5b3b2a0c41904bb40f03b8090fae1a9eb2c7a698
learnReactgithub <sonukushwah362@gmail.com>
2022-11-12 10:14:15 +0530

    react multiple component in single component file

---
+++ src/component/child.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Child() {
+  return (
+    <div>Child component</div>
+  )
+}
+
+function Child2() {
+    return (
+      <div>Child 2 component</div>
+    )
+}
+
+function Child3() {
+    return (
+      <div>Child 3 component</div>
+    )
+}
+export  {Child,Child2,Child3};
